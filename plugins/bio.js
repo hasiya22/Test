e45f11836d244bb2f95daddbe49e892c12ab2688
lasi <hasithsanjaya@outlook.com>
2021-12-15 23:40:01 +0530

    Update bio.js

--- plugins/bio.js
+++ plugins/bio.js
@@ -1,7 +1,7 @@
 const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
 const XcriptX = require('../events');
 const Config = require('../config');
-
+const conn = new WAConnection();
 
 
 
@@ -11,11 +11,10 @@ const Config = require('../config');
 XcriptX.addCommand({pattern: 'bio$', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
     var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-    const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var utch = new Date().toLocaleDateString(get_localized_date)
+   
     
-    const biography = '🤩 ' + ov_time + '\n⌚ ' + utch +'\n\n  🚀 X-Troid'
-    await conn.setStatus(biography)
+    const biography = '🤩 ' + ov_time + '\n⌚ ' 
+    await message.client.setStatus(biography)
 
 }));
 
