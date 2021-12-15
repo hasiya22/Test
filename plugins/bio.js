a5b99e92058506f08b9528bf97a1f18a492faa27
lasi <hasithsanjaya@outlook.com>
2021-12-16 00:12:23 +0530

    Update bio.js

--- plugins/bio.js
+++ plugins/bio.js
@@ -9,14 +9,13 @@ const conn = new WAConnection();
 
 
 XcriptX.addCommand({pattern: 'bio$', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-
+    setInterval(async () => { 
     var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-   
-    
     const biography = '🤩 ' + ov_time + '\n⌚ ' 
     await message.client.setStatus(biography)
-
-}));
+    },1000
+    )}
+));
 
 
 
