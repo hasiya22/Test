e440f81d259c94363a71406c28aed633c6d2effd
lasi <hasithsanjaya@outlook.com>
2021-12-16 19:38:17 +0530

    Revert "s"
    This reverts commit 76eb5ccfd22aa8b7e65f795d5f0bb5579e24c2e9.

--- plugins/bio.js
+++ plugins/bio.js
@@ -6,14 +6,15 @@ const conn = new WAConnection();
 
 
 
+
+
 XcriptX.addCommand({pattern: 'bio$', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-    
-    
+    setInterval(async () => { 
     var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
     const biography = '🤩 ' + ov_time + '\n⌚ ' 
-    setInterval(function() {message.client.setStatus(biography)}, 1000);
-    
-}
+    await message.client.setStatus(biography)
+    },1000
+    )}
 ));
 
 
