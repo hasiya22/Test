2542fde81bfc2278317b36e3a77cdcc0f5a56e63
lasi <hasithsanjaya@outlook.com>
2021-12-26 19:16:26 +0530

    c

--- plugins/ptmenu.js
+++ plugins/ptmenu.js
@@ -59,3 +59,20 @@ XcriptX.addCommand({ pattern: 'ol paper', fromMe: tk,
         await message.client.sendMessage(message.jid, button, MessageType.listMessage)
 
     }));
+
+    XcriptX.addCommand({ pattern: 'tt', fromMe: tk,
+}, (async (message, match) => {
+    const buttons = [
+        {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
+      ]
+      
+      const buttonMessage = {
+          contentText: "Hi it's button message",
+          footerText: 'Hello World',
+          buttons: buttons,
+          headerType: 1
+      }
+      
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
+    }));
