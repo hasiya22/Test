4f6ceaa6ba36a68ed75064350d4d544977f034f8
lasi <hasithsanjaya@outlook.com>
2021-12-26 11:46:21 +0530

    Update Ytbtn.js

--- plugins/Ytbtn.js
+++ plugins/Ytbtn.js
@@ -6,23 +6,23 @@ let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../XScript/y2mate')
 const XcriptX = require('../events');
 const thumb = 'https://telegra.ph/file/83b020ea2b5c123593696.jpg'
+const url = 'https://telegra.ph/file/6d63b2efec080255a42a9.jpg'
+const vd  = 'lasi'
 XcriptX.addCommand({pattern: 'yv ?(.*)', fromMe: true,}, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'need word',MessageType.text);  
-    let results = await yts(match[1])
-    let vid = results.all.find(video => video.seconds < 3600)
-    if (!vid) throw 'සොයා ගත නොහැක '
+  
 
 
     let buttons = [
-        {buttonId: '/yta '+vid.url, buttonText: {displayText: 'සින්දුව 🎵'}, type: 1},
-        {buttonId: '/ytv '+vid.url, buttonText: {displayText: 'වීඩියෝව 📼'}, type: 1},
-      {buttonId: '/cancel '+vid.videoId, buttonText: {displayText: 'ඩිලීට් 🗑️'}, type: 1}
+        {buttonId: 'lasi'+url, buttonText: {displayText: 'සින්දුව 🎵'}, type: 1},
+        {buttonId: '/ytv '+url, buttonText: {displayText: 'වීඩියෝව 📼'}, type: 1},
+      {buttonId: '/cancel '+vd, buttonText: {displayText: 'ඩිලීට් 🗑️'}, type: 1}
       ]
 
 
       const buttonsMessage = {
         image: {url: thumb},
-        caption:`BOTKI`.trim(),    footerText: 'Filesize : '+'5',
+        caption:`BOTKI`.trim(),    footerText: 'Filesize : ',
         buttons: buttons,
          headerType: 4
     }
