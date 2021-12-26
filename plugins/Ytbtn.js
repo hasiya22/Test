7d3826413a1627b1a03492df616ec5f49b4bebc6
lasi <hasithsanjaya@outlook.com>
2021-12-26 11:23:12 +0530

    Update Ytbtn.js

--- plugins/Ytbtn.js
+++ plugins/Ytbtn.js
@@ -5,13 +5,13 @@ const yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../XScript/y2mate')
 const XcriptX = require('../events');
+const thumb = 'https://telegra.ph/file/83b020ea2b5c123593696.jpg'
 XcriptX.addCommand({pattern: 'yv ?(.*)', fromMe: true,}, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'need word',MessageType.text);  
     let results = await yts(match[1])
     let vid = results.all.find(video => video.seconds < 3600)
     if (!vid) throw 'සොයා ගත නොහැක '
-    let isVideo = /2$/.test(command)
-    let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
+
 
     let buttons = [
         {buttonId: '/yta '+vid.url, buttonText: {displayText: 'සින්දුව 🎵'}, type: 1},
@@ -22,8 +22,7 @@ XcriptX.addCommand({pattern: 'yv ?(.*)', fromMe: true,}, (async (message, match)
 
       const buttonsMessage = {
         image: {url: thumb},
-        caption:`BOTKINGDOM !
-    අවශ්‍ය මාධ්‍ය පහත බටන් අතරින් තෝරන්න`.trim(),    footerText: 'Filesize : '+filesizeF,
+        caption:`BOTKI`.trim(),    footerText: 'Filesize : '+'5',
         buttons: buttons,
          headerType: 4
     }
